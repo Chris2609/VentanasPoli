0b8549a0add541040a883fe111521f9665f60097
Chris2609 <megustas260904@gmail.com>
2023-03-07 14:25:34 +0100

    commit

--- src/controlador/ControladorGestionarIns.java
+++ src/controlador/ControladorGestionarIns.java
@@ -1,5 +1,19 @@
 package controlador;
 
+import vista.GestionarInscripciones;
+
 public class ControladorGestionarIns {
 
+	GestionarInscripciones gestionarIns = new GestionarInscripciones();
+	
+	public ControladorGestionarIns(GestionarInscripciones gestionarIns) {
+		this.gestionarIns = gestionarIns;
+	}
+	
+	public void inicializar() {
+		gestionarIns.setTitle("Gestor Inscripciones");
+		gestionarIns.setLocationRelativeTo(null);
+		gestionarIns.setVisible(false);
+	}
+	
 }
